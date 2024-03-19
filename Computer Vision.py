80a68110f8d098a9698f1bb37661103404847dc6
Muhammad Rayyan <103200802+mrnust@users.noreply.github.com>
2024-03-19 16:43:23 +0500

    Update Computer Vision.py

--- Computer Vision.py
+++ Computer Vision.py
@@ -5,13 +5,13 @@
 
 from ultralytics import YOLO V8
 
-Load a model
-model = YOLO("yolov8n-cls.pt")  # build a new model from scratch
+# Load a model
+model = YOLO("yolov8n-cls.pt") 
 
-Use the model
+# Use the model
 model.train(data="C:/Users/HP/Desktop/Python/Dog and Cat Image classifier", epochs=20)  # train the model
 
-model.train(data="D:\Datasets for Computer vision projects/Dog and Cat Image classifier", epochs=20)  # train the model
+
 
 
 
